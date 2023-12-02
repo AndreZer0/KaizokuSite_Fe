ab95b476226c731330f70294a2b892a1bc4803e5
Andrea Manna <andy97.am@gmail.com>
2023-12-02 18:48:02 +0100

    ok jumbo

--- src/components/jumbotron/Carousel.jsx
+++ src/components/jumbotron/Carousel.jsx
@@ -11,7 +11,7 @@ const Carosello = () => {
         <Carousel.Item className='first-item item'>
           <div className='overlay'></div>
           <div className='contenitore primo'>
-            <h2>Bertazzo - Biancotto - Audino </h2>
+            <h2>Paolo Bertazzo, Gregorio Biancotto, Edoardo Audino</h2>
             <p>
               Una nobile guerriera e un furfante patentato formano un
               improbabile team che compete nel Gioco della Corona, torneo sacro
@@ -22,23 +22,20 @@ const Carosello = () => {
         <Carousel.Item className='second-item item'>
           <div className='overlay'></div>
           <div className='contenitore secondo'>
-            <h2>Bertazzo - Biancotto - Audino </h2>
+            <h2>Edoardo Follegatti</h2>
             <p>
-              Una nobile guerriera e un furfante patentato formano un
-              improbabile team che compete nel Gioco della Corona, torneo sacro
-              che decreterà il sovrano di un mondo fanta-rinascimentale.
+              In un neo-medioevo, il cavaliere Barnaba viene inviato alla
+              ricerca della Verità, il leggendario artefatto capace di invertire
+              la Regressione, il male che sta progressivamente cancellando la
+              memoria del genere umano.
             </p>
           </div>
         </Carousel.Item>
         <Carousel.Item className='third-item item'>
           <div className='overlay'></div>
           <div className='contenitore terzo item'>
-            <h2>Bertazzo - Biancotto - Audino </h2>
-            <p>
-              Una nobile guerriera e un furfante patentato formano un
-              improbabile team che compete nel Gioco della Corona, torneo sacro
-              che decreterà il sovrano di un mondo fanta-rinascimentale.
-            </p>
+            <h2>Jacopo Pasqualini, Giada Belviso</h2>
+            <p>In attesa di longline</p>
           </div>
         </Carousel.Item>
       </Carousel>
